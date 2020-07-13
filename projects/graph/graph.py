51ac6e6f69c04fa68a432f9cc85526e7c1c6ce92
Jeremy Rogel <jeremyrogel@Jeremys-MacBook-Pro.local>
2020-07-13 14:37:42 -0400

    initial commit

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -6,6 +6,7 @@ from util import Stack, Queue  # These may come in handy
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,13 +14,16 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add[v2]
+        else:
+            raise IndexError('that vertex is non-existent')
 
     def get_neighbors(self, vertex_id):
         """
